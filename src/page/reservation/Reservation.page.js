535a7813cfd95343323f73243d6303c00daa870b
Maelys chassin <m.chassin@altelis.com>
2022-02-01 11:19:55 +0100

    add calendar page

---
+++ src/page/reservation/Reservation.page.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+import Calendar from 'react-calendar';
+
+export const Reservation = () => {
+
+ const [date, setDate] = useState(new Date());
+
+ return(
+   <>
+    <h1>Calendars</h1>
+    <div className="calendar-container">
+      <Calendar onChange={setDate} value={date}/>
+    </div>
+
+    <div className="selected-date">
+     <span>Selected date :{date.toDateString()}</span>
+    </div>
+  </>
+ )
+}
