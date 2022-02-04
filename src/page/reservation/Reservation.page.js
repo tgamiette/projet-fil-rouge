4263ce3e38eb658073524dc8c4bd4de95b4c2574
Maelys chassin <m.chassin@altelis.com>
2022-02-04 10:14:08 +0100

    Calendar add events (without db)

--- src/page/reservation/Reservation.page.js
+++ src/page/reservation/Reservation.page.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
+import DatePicker from "react-datepicker";
 import moment from 'moment';
+import events from './events.js';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import events from './events.js'
+import "react-datepicker/dist/react-datepicker.css";
+import './Reservation.css';
 
 export const Reservation = () => {
 
@@ -12,22 +15,47 @@ export const Reservation = () => {
    <>
     <h1>Calendars</h1>
     <div className="calendar-container">
-      <MyCalendar />
+      <MyCalendar date={date.toDateString()}/>
     </div>
 
     <div className="selected-date">
      <span>Selected date :{date.toDateString()}</span>
     </div>
+
+    <div>{date.toDateString()}</div>
   </>
  )
 }
 
-const MyCalendar = props => {
+const MyCalendar = (todaydate) => {
+
+  const [view, setView] = useState('month');
+
+
+  const [newEvent, setNewEvent] = useState({id: 16, title: '', start: '', end: ''});
+  const [allEvents, setAllEvents] = useState(events);
   // or globalizeLocalizer
   const localizer = momentLocalizer(moment);
+
+  const slotSelected = (slotInfo) => {
+    alert(`start ${slotInfo.start.toLocaleString()}`);
+    setView('day');
+  }
+
+  function handleAddEvent(){
+    setAllEvents([...allEvents, newEvent]);
+  }
+
   return(
     <div className="calendar-container">
-      <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end"/>
+      <div class="c-calendar_form" >
+        <input type="text" name="title" value={newEvent.title} onChange={(e) => setNewEvent({...newEvent, title: e.target.value})} />
+        <DatePicker value={newEvent.start} onChange={(start) =>  setNewEvent({...newEvent, start})}/>
+        <DatePicker value={newEvent.end} onChange={(end) =>  setNewEvent({...newEvent, end})}/>
+        <button type="button" onClick={handleAddEvent}>Add Events</button>
+      </div>
+      <Calendar selectable localizer={localizer} events={allEvents} defaultView={view} startAccessor="start" endAccessor="end"
+      onSelectSlot={(slotInfo) => slotSelected(slotInfo)} style={{height: 500}}/>
     </div>
   )
 }
