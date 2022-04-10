372096db5174ad961abde0877591d292c962e9c2
teddy <tgamiette@kernix.com>
2022-04-10 15:46:00 +0200

    install jwt (ok)

--- app/symfony/src/Entity/User.php
+++ app/symfony/src/Entity/User.php
@@ -188,4 +188,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface {
         return $this;
     }
 
+    public function getUsername(): string {
+        return $this->email;
+    }
+
 }
