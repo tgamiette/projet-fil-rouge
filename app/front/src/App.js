16cbcf2dec7c38311e62504c0d868ab498b061fd
Maelys chassin <m.chassin@altelis.com>
2022-04-23 15:32:08 +0200

    [fix] fix react install

--- app/front/src/App.js
+++ app/front/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>Le test qdss</p>
     </div>
   );
 }
