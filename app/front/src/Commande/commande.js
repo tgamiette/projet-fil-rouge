523403536619910890d4eea030c67aea6ac00b15
Maelys chassin <m.chassin@altelis.com>
2022-04-27 09:56:25 +0200

    [add] api request + jwt token

--- app/front/src/Commande/commande.js
+++ app/front/src/Commande/commande.js
@@ -1,9 +1,20 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import { useWaitFor } from "../shared/hooks";
+import {get_all_product} from '../api';
 import './style/commande.css';
 
 
 export default function Commande(){
+  const [token, setToken] = useState(false);
+
+  useWaitFor(
+    () => get_all_product(),
+    [],
+    (res) => {
+      console.log('products', res);
+
+    }
+  );
 
   return (
     <h1>Commande</h1>
--- app/front/src/Main/signIn.js
+++ app/front/src/Main/signIn.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {add_user} from '../api';
+import { useWaitFor } from "../shared/hooks";
 
 export default function SignIn(){
+
+  const [user, setUser] = useState({username: "", password: "", email: ""});
+
+  useWaitFor(
+     () => add_user(user.email, user.password, user.username),
+     [user],
+     (res) => {
+       console.log('user', res);
+     }
+   );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({
+      email: e.target.email.value,
+      password: e.target.password.value,
+      username: `${e.target.prenom.value} ${e.target.nom.value}`
+    });
+  }
 
  return(
   <>
    <h1>SignIn</h1>
+
+   <form className="" onSubmit={handleSubmit}>
+    <label className="">Nom</label>
+    <input type="text" name="nom"  />
+
+    <label className="">Prénom</label>
+    <input type="text" name="prenom"  />
+
+    <label className="">Email</label>
+    <input type="email" name="email"  />
+
+    <label className="">Password</label>
+    <input className="" type="password" name="password"  />
+
+    <button className="" type="submit">S'inscrire</button>
+   </form>
   </>
  )
 }
