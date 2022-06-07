b25aec7d58350922cdef88af96393c9517c75998
Maelys chassin <m.chassin@altelis.com>
2022-06-07 21:28:00 +0200

    [fix] server symfony

--- app/symfony/src/Entity/Product.php
+++ app/symfony/src/Entity/Product.php
@@ -20,7 +20,6 @@ use Symfony\Component\Validator\Constraints as Assert;
     ],
     itemOperations: [
         'GET',
-        'POST',
         'PUT' => [
             "security_message" => "Tu n'as pas les droits sur ce produit ",
             "security" => "is_granted('ROLE_ADMIN') and object.seller == user",
