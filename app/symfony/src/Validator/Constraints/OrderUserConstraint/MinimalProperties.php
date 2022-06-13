f450f417e60082a11fc0f8aebf6a0e34598a3934
teddy <tgamiette@kernix.com>
2022-06-13 10:30:42 +0200

    .

---
+++ app/symfony/src/Validator/Constraints/OrderUserConstraint/MinimalProperties.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Validator\Constraints\OrderUserConstraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
+class MinimalProperties extends Constraint
+{
+    /*
+     * Any public properties become valid options for the annotation.
+     * Then, use these in your validator class.
+     */
+    public $message = 'The value "{{ value }}" is not valid.';
+}
