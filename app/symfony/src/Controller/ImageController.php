bd45e05aa62d6b1538db4c5db759ab8fda9fb1f2
teddy <tgamiette@kernix.com>
2022-07-05 12:20:16 +0200

    review

--- app/symfony/src/Controller/ImageController.php
+++ app/symfony/src/Controller/ImageController.php
@@ -19,7 +19,7 @@ class ImageController extends AbstractController {
     }
 
     /**
-     * @Route("/public/asset/images/product/{name}", name="index")
+     * @Route("/images/products/{name}", name="index")
      */
     public function index(string $name, Request $request, Security $security) {
 
